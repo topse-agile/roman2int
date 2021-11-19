ad084c89f1fa36b0c4e6bf279ca8bcb8b1b14ea3
Takuo Doi <takuo.doi@gmail.com>
2021-11-20 05:33:02 +0900

    Initial commit

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FizzBuzzTest {
+    @Test
+    public void testSomething() {
+        assertThat(1, is(1));
+    }
+}
